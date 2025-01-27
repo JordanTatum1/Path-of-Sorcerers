87c39ae01a8c49f5a5de84e1d96a900afc7a362e
JordanTatum1 <159198043+JordanTatum1@users.noreply.github.com>
2025-01-27 11:51:05 -0800

    Hands not shooting bullets

---
+++ M10.twin_stick_workbook/bullet.gd
@@ -0,0 +1,20 @@
+class_name bullet extends Area2D
+
+@export var speed := 750
+
+var max_range := 1000.0
+
+var _traveled_distance = 0.0
+
+
+func _physics_process(delta: float) -> void:
+	var distance := Vector2.RIGHT.rotated(rotation) 
+	position += distance * speed * delta
+	
+	_traveled_distance += speed * delta
+	if _traveled_distance > max_range:
+		_destroy()
+
+
+func _destroy():
+	queue_free()
